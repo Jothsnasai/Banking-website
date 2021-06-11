3a8a000128f437460e946828a9f373050c248fb7
Jothsnasai <61556094+Jothsnasai@users.noreply.github.com>
2021-06-11 16:25:15 +0530

    Create Homesite.php

---
+++ FrontEnd/Homesite.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+           <div class="navbar">
+               <img src="images/Banklogo1.png" class="logo">
+               <nav>
+                   <ul>
+                   <li><a href="viewcustomers.php">View Customers</a></li>
+                   <li><a href="viewtransactions.php">View Transactions</a></li>
+                   </ul>
+               </nav>
+           </div>
+           <div class="content">
+               <h1>The Basic Bank</h1>
+               <p>Secure Payment</p>
+           </div>
+           <img src="images/bankimage.jpg" class="feature-img">
+   </div>
+</body>
+</html>
